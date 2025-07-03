671f822bc1cf0a6eab04f09596ba30be8296c0bf
sodiq abdullahi <sodiqabdullahi431@gmail.com.com>
2025-07-03 16:30:44 +0100

    private part edit new

--- src/Components/Viewonemaleprivatemalehostel.jsx
+++ src/Components/Viewonemaleprivatemalehostel.jsx
@@ -20,6 +20,8 @@ import {
   fetchingAllprivatemalehostelSuccessful,
 } from "./Redux/Allprivatemalehostel";
 
+import imageUrl2 from "../assets/image1_large.jpg";
+
 const Viewonemaleprivatemalehostel = () => {
   const [loading, setloading] = useState(false);
 
@@ -97,21 +99,19 @@ const Viewonemaleprivatemalehostel = () => {
 
       if (response.data && response.data.data.authorization_url) {
         window.location.href = response.data.data.authorization_url; // Redirect to Paystack
-
       }
     } catch (error) {
       console.error("Error applying for room", error);
       setloading(false);
-
     }
   };
+  const imageUrl = imageUrl2;
 
   return (
     <>
+      {/* loading  */}
 
-{/* loading  */}
-
-{loading && (
+      {loading && (
         <div className="looder_body">
           <span className="loader"></span>
         </div>
@@ -127,93 +127,119 @@ const Viewonemaleprivatemalehostel = () => {
         </div>
       ) : (
         <section>
-          <div className=" d-flex justify-content-evenly oneprivatehouseholder flex-wrap ">
-            <div className=" col-12 col-lg-4 d-flex justify-content-center align-items-center">
-              <img
-                className="  oneprivatehouseholderimg"
-                src={allprivatemalehostel[id].img_array}
-                alt=""
-              />
-            </div>
-            <div className=" ms-3 col-12 col-lg-6  pt-3 ">
-              <p className=" onelinetext text-capitalize fw-semibold ">
-                <span className=" text-uppercase fw-bold">building name:</span>{" "}
-                {allprivatemalehostel[id].building_name}
-              </p>
-              <p className=" onelinetext text-capitalize fw-semibold ">
-                <span className=" text-uppercase fw-bold">
-                  building address:
-                </span>{" "}
-                {allprivatemalehostel[id].building_address}
-              </p>
-              <p className=" onelinetext text-capitalize fw-semibold ">
-                <span className=" text-uppercase fw-bold">
-                  numbers of active room:
-                </span>{" "}
-                {allprivatemalehostel[id].room_count}
-              </p>
-              <p className=" onelinetext text-capitalize fw-semibold ">
-                <span className=" text-uppercase fw-bold">
-                  {" "}
-                  rent per room :
+          {/*  */}
+          <div
+            className="bg-cover col-11 m-auto bg-center flex flex-col mb-5 justify-end overflow-hidden bg-white testingnewcompo2"
+            style={{
+              backgroundImage: `
+        linear-gradient(0deg, rgba(0, 0, 0, 0.4) 0%, rgba(0, 0, 0, 0) 25%),
+        url(${
+          allprivatemalehostel[id].img_array !== ""
+            ? allprivatemalehostel[id].img_array
+            : imageUrl
+        })
+    `,
+            }}
+          >
+            <div className="flex p-4 testingnewcompodiv">
+              <p className=" text-light fw-bold fs-4 m-0">
+                <span className=" text-capitalize">
+                  NAME: {allprivatemalehostel[id].building_name}
                 </span>
-                ₦ {allprivatemalehostel[id].rent}
-              </p>
-              <p className=" onelinetext text-capitalize fw-semibold ">
-                <span className=" text-uppercase fw-bold">
-                  {" "}
-                  one room capacity:
-                </span>{" "}
-                {allprivatemalehostel[id].one_room_capacity} per room
-              </p>
-              <p className=" onelinetext text-capitalize fw-semibold ">
-                <span className=" text-uppercase fw-bold">
-                  {" "}
-                  room description :
-                </span>{" "}
-                {allprivatemalehostel[id].room_description}
-              </p>
-              <p className=" onelinetext text-capitalize fw-semibold ">
-                <span className=" text-uppercase fw-bold"> hostel rules :</span>{" "}
-                {allprivatemalehostel[id].rules}
               </p>
             </div>
           </div>
-          <br />
-          <hr />
+          {/*  */}
 
-          <div className=" mb-2  p-1  ">
-            <p className=" m-0  smalltextnote fst-italic fw-bold font">
-              <span className=" text-capitalize fs-6">Checkbox behavior</span>:
-              If a checkbox is checked, it means that the corresponding room is
-              occupied. If it's unchecked, the room is unoccupied.
-            </p>
-          </div>
+          <div className="oneprivatehouseholder">
+           <div className=" p-2">
+             <p className=" fs-4 fw-bold text-capitalize"> building details</p>
+            <div className=" mb-2  p-1  ">
+              <p className=" m-0  smalltextnote fst-italic fw-bold font">
+                <span className=" text-capitalize">Checkbox behavior </span>: If
+                a checkbox is checked, it means that the corresponding room is
+                occupied. If it's unchecked, the room is unoccupied.
+              </p>
+            </div>
+           </div>
+            <div className=" d-flex oneprivatehouseholderbox flex-wrap  ">
+              <div className="  oneprivatehouseholderbox1 p-2 col-12 col-md-4 ">
+                <p className=" onelinetext text-capitalize fw-semibold ">
+                  <span className=" text-uppercase fw-bold">
+                    building name:
+                  </span>{" "}
+                  {allprivatemalehostel[id].building_name}
+                </p>
+                <p className=" onelinetext text-capitalize fw-semibold ">
+                  <span className=" text-uppercase fw-bold">
+                    building address:
+                  </span>{" "}
+                  {allprivatemalehostel[id].building_address}
+                </p>
+                <p className=" onelinetext text-capitalize fw-semibold ">
+                  <span className=" text-uppercase fw-bold">
+                    numbers of active room:
+                  </span>{" "}
+                  {allprivatemalehostel[id].room_count}
+                </p>
+                <p className=" onelinetext text-capitalize fw-semibold ">
+                  <span className=" text-uppercase fw-bold">
+                    {" "}
+                    rent per room :
+                  </span>
+                  ₦ {allprivatemalehostel[id].rent}
+                </p>
+                <p className=" onelinetext text-capitalize fw-semibold ">
+                  <span className=" text-uppercase fw-bold">
+                    {" "}
+                    one room capacity:
+                  </span>{" "}
+                  {allprivatemalehostel[id].one_room_capacity} per room
+                </p>
+                <p className=" onelinetext text-capitalize fw-semibold ">
+                  <span className=" text-uppercase fw-bold">
+                    {" "}
+                    room description :
+                  </span>{" "}
+                  {allprivatemalehostel[id].room_description}
+                </p>
+                <p className=" onelinetext text-capitalize fw-semibold ">
+                  <span className=" text-uppercase fw-bold">
+                    {" "}
+                    hostel rules :
+                  </span>{" "}
+                  {allprivatemalehostel[id].rules}
+                </p>
+              </div>
 
-          <div className=" d-flex flex-wrap ">
-            {allprivatemalehostel[id].rooms.map((item, index) => (
-              <div className="col-6 col-md-4 col-lg-3 my-2" key={index}>
-                <div
-                  data-bs-toggle="modal"
-                  data-bs-target={
-                    item.occupant ? "#notVacantModal" : "#applyModal"
-                  }
-                  onClick={() => handleBunkerClick(item)}
-                  className="oneprivatehouseholderroom d-flex p-2 col-11 rounded justify-content-evenly align-items-center"
-                >
-                  <p className="m-0 text-capitalize fs-5 fw-bold">
-                    Room: {item.room_id}
-                  </p>
-                  <input
-                    type="checkbox"
-                    checked={item.availability} // Checked if room is available
-                    name=""
-                    id=""
-                    readOnly // Ensure it's not modified if it's meant to be display-only
-                  />
+              <div className=" col-12 col-md-8 p-2">
+                <div className=" d-flex flex-wrap ">
+                  {allprivatemalehostel[id].rooms.map((item, index) => (
+                    <div className="col-6 col-md-4 col-lg-3 my-2" key={index}>
+                      <div
+                        data-bs-toggle="modal"
+                        data-bs-target={
+                          item.occupant ? "#notVacantModal" : "#applyModal"
+                        }
+                        onClick={() => handleBunkerClick(item)}
+                        className="oneprivatehouseholderroom d-flex p-2 col-11 rounded justify-content-evenly align-items-center"
+                      >
+                        <p className="m-0 text-capitalize fs-5 fw-bold">
+                          Room: {item.room_id}
+                        </p>
+                        <input
+                          type="checkbox"
+                          checked={item.availability} // Checked if room is available
+                          name=""
+                          id=""
+                          readOnly // Ensure it's not modified if it's meant to be display-only
+                        />
+                      </div>
+                    </div>
+                  ))}
                 </div>
               </div>
-            ))}
+            </div>
           </div>
         </section>
       )}
